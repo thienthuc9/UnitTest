a61d53be82a35c741033e926bc8c1266f167344d
thienthuc9 <phandinhthethien@gmail.com>
2019-04-18 09:59:46 +0700

    conmuangangqua

---
+++ WebApplication1/WebApplication1/Controllers/CHSController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1;
+
+namespace WebApplication1.Controllers
+{
+    public class CHSController : Controller
+    {
+        private CHSEntities db = new CHSEntities();
+
+        // GET: /CHS/
+        public ActionResult Index()
+        {
+            return View(db.Table_1.ToList());
+        }
+
+        // GET: /CHS/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table_1 table_1 = db.Table_1.Find(id);
+            if (table_1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table_1);
+        }
+
+        // GET: /CHS/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /CHS/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="MaSach,TenSach,Giá,TG,SL")] Table_1 table_1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Table_1.Add(table_1);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table_1);
+        }
+
+        // GET: /CHS/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table_1 table_1 = db.Table_1.Find(id);
+            if (table_1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table_1);
+        }
+
+        // POST: /CHS/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="MaSach,TenSach,Giá,TG,SL")] Table_1 table_1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(table_1).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table_1);
+        }
+
+        // GET: /CHS/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table_1 table_1 = db.Table_1.Find(id);
+            if (table_1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table_1);
+        }
+
+        // POST: /CHS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table_1 table_1 = db.Table_1.Find(id);
+            db.Table_1.Remove(table_1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
